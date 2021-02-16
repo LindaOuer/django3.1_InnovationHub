fb58dd499f47651e37025e1973ab47ceb155bd0e
LindaOuer <linda.ouerfelli@gmail.com>
2021-02-16 09:00:14 +0100

    Custom StudentAdmin + Added created_at & updated_at to Project Model

--- hub/admin.py
+++ hub/admin.py
@@ -3,7 +3,38 @@ from .models import Student, Coach, User, Project
 
 # Register your models here.
 
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['project_name']
+            }
+        )
+    ]
+    extra = 0
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'email'
+    )
+    fields = (
+        ('last_name', 'first_name'),
+        'email'
+    )
+    inlines = [
+        ProjectInline
+    ]
+
+
+admin.site.register(Student, StudentAdmin)
+
 admin.site.register(User)
 admin.site.register(Coach)
-admin.site.register(Student)
+
 admin.site.register(Project)
