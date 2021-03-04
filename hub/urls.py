1e844eca83354dcae4a6206dd7b348c1ec212427
LindaOuer <linda.ouerfelli@gmail.com>
2021-03-04 21:14:16 +0100

    Render - detail project

--- hub/urls.py
+++ hub/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homePage, list_Projects
+from .views import homePage, list_Projects, details_project
 
 urlpatterns = [
     path('home', homePage, name="home"),
-    path('', list_Projects, name="list_projects")
+    path('', list_Projects, name="list_projects"),    
+    path('project/<int:id>', details_project, name="details_project"),    
 ]
--- hub/views.py
+++ hub/views.py
@@ -4,15 +4,29 @@ from django.http import HttpResponse
 from .models import Project
 
 # function based views
+
+
 def homePage(request):
     return HttpResponse('<h1>Welcome To ... </h1>')
+
 
 def list_Projects(request):
     list = Project.objects.all()
     return render(
-        request, 
+        request,
         'hub/index.html',
         {
             'projects': list,
         },
-        )
+    )
+
+
+def details_project(request, id):
+    project = Project.objects.get(pk=id)
+    return render(
+        request,
+        'hub/details_project.html',
+        {
+            'project': project,
+        }
+    )
